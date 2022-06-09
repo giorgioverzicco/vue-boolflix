5b50ae3d382a62add732e31e9d1544f05f6e099e
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-06-09 12:58:45 +0200

    add flags

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -5,7 +5,7 @@
       <li v-for="result in results" :key="result.id">
         <h1>{{ result.title }}</h1>
         <h2>{{ result.original_title }}</h2>
-        <p>{{ result.original_language }}</p>
+        <CountryFlag :country="getCountry(result.original_language)" />
         <p>{{ result.vote_average }}</p>
       </li>
     </ul>
@@ -13,14 +13,34 @@
 </template>
 
 <script>
+import CountryFlag from 'vue-country-flag'
+
 import SearchBar from '../molecules/SearchBar.vue'
+
+const CountryLanguage = require('country-language')
 export default {
   name: 'TheHeader',
-  components: { SearchBar },
+  components: { CountryFlag, SearchBar },
   data() {
     return {
       results: [],
     }
+  },
+  methods: {
+    getCountry(lang) {
+      return CountryLanguage.getLanguage(lang, (e, language) => {
+        const result = language.countries.at(-1).code_2.toLowerCase()
+
+        switch (result) {
+          case 'ag':
+            return 'gb'
+          case 'ar':
+            return 'es'
+          default:
+            return result
+        }
+      })
+    },
   },
 }
 </script>
