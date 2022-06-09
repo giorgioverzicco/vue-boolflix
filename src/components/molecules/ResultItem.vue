2be871ff8b75f692895eea716fe13814fe6be791
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-06-09 14:33:59 +0200

    refactor result item

--- src/components/molecules/ResultItem.vue
+++ src/components/molecules/ResultItem.vue
@@ -1,11 +1,10 @@
 <template>
   <li>
-    <BasePoster :url="result.poster_path" size="w185" />
-    <BaseTitle>{{ getData(result).title }}</BaseTitle>
-    <BaseTitle :tag="h2">{{ getData(result).original_title }}</BaseTitle>
-    <BaseText>{{ result.original_language }}</BaseText>
-    <CountryFlag :country="getData(result).language" />
-    <BaseText>{{ getData(result).vote_average }}</BaseText>
+    <BasePoster :url="data.poster_path" size="w185" />
+    <BaseTitle>{{ data.title }}</BaseTitle>
+    <BaseTitle :tag="h2">{{ data.original_title }}</BaseTitle>
+    <CountryFlag :country="data.language" />
+    <BaseText>{{ data.vote_average }}</BaseText>
   </li>
 </template>
 
@@ -26,31 +25,36 @@ export default {
       required: true,
     },
   },
-  methods: {
-    getData(result) {
+  computed: {
+    data() {
       return {
-        title: result.title || result.name,
-        original_title: result.original_title || result.original_name,
-        language: this.getCountry(result.original_language),
-        vote_average: Math.ceil(result.vote_average / 2),
+        poster_path: this.result.poster_path,
+        title: this.result.title || this.result.name,
+        original_title: this.result.original_title || this.result.original_name,
+        language: this.getCountry(),
+        vote_average: this.result.vote_average,
       }
     },
+  },
+  methods: {
+    getCountry() {
+      return CountryLanguage.getLanguage(
+        this.result.original_language,
+        (e, language) => {
+          const result = language.countries[0].code_2.toLowerCase()
 
-    getCountry(lang) {
-      return CountryLanguage.getLanguage(lang, (e, language) => {
-        const result = language.countries[0].code_2.toLowerCase()
-
-        switch (result) {
-          case 'ag':
-            return 'gb'
-          case 'ar':
-            return 'es'
-          case 'be':
-            return 'fr'
-          default:
-            return result
+          switch (result) {
+            case 'ag':
+              return 'gb'
+            case 'ar':
+              return 'es'
+            case 'be':
+              return 'fr'
+            default:
+              return result
+          }
         }
-      })
+      )
     },
   },
 }
